90f82b1dfae5963c45db5ac7052bf42b5615fa27
gfanton <8671905+gfanton@users.noreply.github.com>
2021-05-16 20:28:55 +0200

    chore: update spacemacs, zsh plugins, darwin, home-manager, nixpkgs
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- home/default.nix
+++ home/default.nix
@@ -278,5 +278,5 @@ in
   #
   # You can update Home Manager without changing this value. See the Home Manager release notes for
   # a list of state version changes in each release.
-  home.stateVersion = "21.03";
+  home.stateVersion = "21.05";
 }
