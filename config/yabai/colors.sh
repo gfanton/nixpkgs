540af9eadb6c4ae8f5192ea425b992a9ddf42c63
gfanton <8671905+gfanton@users.noreply.github.com>
2023-08-21 23:01:24 +0200

    feat: update yabai & skhd
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

---
+++ config/yabai/colors.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+# black
+COLOR_BLACK="181926" # Default Black / mantle
+COLOR_BRIGHT_BLACK="1e2030" # Bright Black / crust
+# red
+COLOR_RED="ed8796" # Default Red / Red
+COLOR_BRIGHT_RED="ee99a0" # Bright Red / Maroon
+# green
+COLOR_GREEN="a6da95" # Default Green / Green
+COLOR_BRIGHT_GREEN="8bd5ca" # Bright Green / Teal
+# yellow
+COLOR_YELLOW="f5a97f" # Default Yellow / Peach
+COLOR_BRIGHT_YELLOW="eed49f" # Bright Yellow / Yellow
+# blue
+COLOR_BLUE="8aadf4" # Default Blue / Blye
+COLOR_BRIGHT_BLUE="b7bdf8" # Bright Blue / Lavender
+# magenta
+COLOR_MAGENTA="f5bde6" # Default Magenta / Pink
+COLOR_BRIGHT_MAGENTA="c6a0f6" # Bright Magenta / Mauve
+# cyan
+COLOR_CYAN="91d7e3" # Default Cyan / Sky
+COLOR_BRIGHT_CYAN="7dc4e4" # Bright Cyan / Sapphire
+# white
+COLOR_WHITE="f4dbd6" # Default White / Rosewater
+COLOR_BRIGHT_WHITE="f0c6c6" # Bright White / Flamingo
+
+# other
+COLOR_TEXT="cad3f5"
+COLOR_SUBTEXT1="b8c0e0"
+COLOR_SUBTEXT0="a5adcb"
+COLOR_OVERLAY2="939ab7"
+COLOR_OVERLAY1="8087a2"
+COLOR_OVERLAY0="6e738d"
+COLOR_SURFACE2="5b6078"
+COLOR_SURFACE1="494d64"
+COLOR_SURFACE0="363a4f"
+COLOR_BASE="24273a"
+COLOR_MANTLE="1e2030"
+COLOR_CRUST="181926"
